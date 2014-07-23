d4a9339b182c2f2cf230517453da2c658572d10b
Eric Rasmussen <ericrasmussen@gmail.com>
2014-07-23 13:15:31 -0700

    code cleanup

--- src/demos/compiled/conditional/Text.hs
+++ src/demos/compiled/conditional/Text.hs
@@ -1,12 +1,5 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-------------------------------------------------------------------------------
--- | The purpose of this module is to show off various ways to conditionally
--- include content in Heist templates. You will see some copy/paste and shared
--- code here in other modules, but the intent is to make this a standalone
--- module so you don't need to follow a bunch of imports in order to understand
--- these examples.
-
 module Demos.Compiled.Conditional.Text
   ( conditionalHandler
   , condTextSplices
@@ -30,16 +23,23 @@ import           Application
 import           Demos.Utils.Templates (makeTemplateSplices)
 --------------------------------------------------------------------------------
 
--- simple data type for Tutorials that may or may not have an author
+-- -----------------------------------------------------------------------------
+-- * Define your data
+
 data Tutorial = Tutorial {
     title  :: T.Text
   , url    :: T.Text
   , author :: Maybe T.Text
   }
 
--- unlike the interpreted version, we want values of Tutorial elevated to
--- RuntimeSplice n Tutorial so they can be passed in to compiled splices. We
--- could use this same pattern for values pulled from a database or other source
+-- helper function to show the conditional case: having a credited author or not
+maybeAuthor :: Tutorial -> T.Text
+maybeAuthor t = case author t of
+  Nothing -> "no credited author"
+  Just a  -> a
+
+
+-- create two sample Tutorials
 tutorialA :: Monad n => RuntimeSplice n Tutorial
 tutorialA = return Tutorial {
     title  = "Heist Template Tutorial"
@@ -54,54 +54,42 @@ tutorialB = return Tutorial {
   , author = Just "mightybyte"
   }
 
---------------------------------------------------------------------------------
--- * A handler to demonstrate conditional text attached to a template node
 
--- | This is the Snap Handler that will be associated with a route in
--- src/Site.hs
-conditionalHandler :: Handler App App ()
-conditionalHandler = cRender "conditional/text/condtext"
+-- -----------------------------------------------------------------------------
+-- * Create splices for each the tutorials
 
--- | Creates compiled splices for a template that expects two different
--- tutorials to be inserted. We could map across a list of Tutorials, but to
--- keep the example simple the two tutorials are inserted one by one. You can
--- see a more advanced (and practical) example in src/handlers/LoopCompiled.hs
---
--- Note: Splices form a monoid so we can combine the two sets of splices with
--- mappend
-tutorialSplices :: Monad n => Splices (C.Splice n)
-tutorialSplices = applyS tutorialA splicesA `mappend` applyS tutorialB splicesB
-
--- | Creates compiled splices that can be applied to a runtime Tutorial splice
--- to bind the splices to the values of that tutorial
+-- Note: you do not need to create separate splices for each author, and you can
+-- see examples of this in the Heist loops demo. We create separate splices for
+-- each tutorial here explicitly to illustrate conditional author field.
 splicesA :: Monad n => Splices (RuntimeSplice n Tutorial -> C.Splice n)
 splicesA = mapS (C.pureSplice . C.textSplice) $ do
   "titleA"       ## title
   "urlA"         ## url
   "maybeAuthorA" ## maybeAuthor
 
--- | Same as splicesA, repeated here to belabor the point
 splicesB :: Monad n => Splices (RuntimeSplice n Tutorial -> C.Splice n)
 splicesB = mapS (C.pureSplice . C.textSplice) $ do
   "titleB"       ## title
   "urlB"         ## url
   "maybeAuthorB" ## maybeAuthor
 
--- | Returns conditional Text based on whether or not we know the tutorial's
--- author
-maybeAuthor :: Tutorial -> T.Text
-maybeAuthor t = case author t of
-  Nothing -> "no credited author"
-  Just a  -> a
+-- combine all the splices we've defined here
+tutorialSplices :: Monad n => Splices (C.Splice n)
+tutorialSplices = applyS tutorialA splicesA `mappend` applyS tutorialB splicesB
 
 
--- ok so condtext.tpl is its own thing now
--- but we also need to name where this source file will end up and render it too
--- then change Templates.hs to be able to find both
+
+-- -----------------------------------------------------------------------------
+-- * Create compiled Heist splices to export
+
+-- takes the splices defined above and `mconcat`s them with display tab splices
 condTextSplices :: Monad m => Splices (C.Splice m)
 condTextSplices = mconcat [ tutorialSplices
                           , makeTemplateSplices "text" "condTextTabs"
                           ]
 
+--------------------------------------------------------------------------------
+-- * Create a handler to render the Heist template
 
-
+conditionalHandler :: Handler App App ()
+conditionalHandler = cRender "conditional/text/condtext"
